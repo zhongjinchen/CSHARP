f6d3364dea04ccf1b16cf3a9e5401f2ea47507aa
薛明林 <Administrator@PG103430FI9AVFF>
2019-07-04 15:50:04 +0800

    add code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace CSHARP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 1, 3, 7, 13, 37, 47, 69, 79, 158, 378, 489 };
+            Console.WriteLine("现有数组；1, 3, 7, 13, 37, 47, 69, 79, 158, 378, 489");
+
+            int index = array.Length - 1;
+            Console.WriteLine("请输入你要查找的数字");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < array. Length; i++)
+            {
+                int steps = (index + 1) / 2;
+                if (input == array[index])
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"找到了，共寻找{i}次，在第{index + 1 }个位置");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"第{i}次寻找，取第{index + 1 }个数,值为{array[index]}");
+                    if (input > array[index])
+                    {
+                        index += steps;
+                    }
+                    else
+                    {
+                        index -= steps;
+                    }
+                    Console.WriteLine($"设下一次查找的位置为{index + 1}");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
